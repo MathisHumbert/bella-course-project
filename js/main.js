06e0695fa886944a614faf57f05da8eae66a7ead
MathisHumbert <84037244+MathisHumbert@users.noreply.github.com>
2022-10-05 16:51:19 +0200

    parallax images and pin steps are done

--- js/main.js
+++ js/main.js
@@ -215,6 +215,77 @@ function initPortfolioHover() {
 
 /* 
 ======
+PARALLAX IMAGES AND PIN STEPS
+======
+*/
+function initImageParallax() {
+  gsap.utils.toArray('.with-parallax').forEach((section) => {
+    const image = section.querySelector('img');
+    gsap.to(image, {
+      yPercent: 20,
+      // top: '20%'
+      ease: 'none',
+      scrollTrigger: {
+        trigger: section,
+        start: 'top bottom',
+        scrub: 1,
+      },
+    });
+  });
+}
+
+function initPinSteps() {
+  ScrollTrigger.create({
+    trigger: '.fixed-nav',
+    start: 'top center',
+    endTrigger: '#stage4',
+    end: 'center center',
+    pin: true,
+  });
+
+  const navLinks = gsap.utils.toArray('.fixed-nav li');
+
+  const getVh = () => {
+    const vh = Math.max(
+      document.documentElement.clientHeight || 0,
+      window.innerHeight || 0
+    );
+    return vh;
+  };
+
+  const updateBodyColor = (color) => {
+    // using gsap
+    // gsap.to('.fill-background', { backgroundColor: color, ease: 'none' });
+
+    document.documentElement.style.setProperty('--bcg-fill-color', color);
+  };
+
+  gsap.utils.toArray('.stage').forEach((stage, index) => {
+    ScrollTrigger.create({
+      trigger: stage,
+      start: 'top center',
+      // function base value that update when ScrollTrigger refresh
+      end: () => `+=${stage.clientHeight + getVh() / 10}`,
+      toggleClass: {
+        targets: navLinks[index],
+        className: 'is-active',
+      },
+      onEnter: () => updateBodyColor(stage.dataset.color),
+      onEnterBack: () => updateBodyColor(stage.dataset.color),
+    });
+  });
+
+  ScrollTrigger.create({
+    trigger: '#stage1',
+    start: 'top center',
+    end: 'top center',
+    markers: true,
+    onLeaveBack: () => updateBodyColor('#acb7ae'),
+  });
+}
+
+/* 
+======
 INIT FUNCTION
 ======
 */
@@ -222,6 +293,8 @@ function init() {
   initNavigation();
   initHeaderTilt();
   initPortfolioHover();
+  initImageParallax();
+  initPinSteps();
 }
 
 /* 
