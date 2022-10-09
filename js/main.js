b655bca6a4894665d24b26b259faba73b0a568b0
MathisHumbert <84037244+MathisHumbert@users.noreply.github.com>
2022-10-09 10:55:16 +0200

    loader created

--- js/main.js
+++ js/main.js
@@ -330,6 +330,108 @@ function initSmoothScrollbar() {
 
 /* 
 ======
+LOADER
+======
+*/
+// const loader = document.querySelector('.loader');
+// const loaderInner = document.querySelector('.loader .inner');
+// const progressBar = document.querySelector('.loader .progress');
+
+// // show loader on page
+// gsap.set(loader, { autoAlpha: 1 });
+
+// // scale loader down
+// gsap.set(loaderInner, { scaleY: 0.005, transformOrigin: 'bottom' });
+
+// // make a tween taht scales the loader
+// const progressTween = gsap.to(progressBar, {
+//   scaleX: 0,
+//   ease: 'none',
+//   transformOrigin: 'right',
+//   paused: true,
+// });
+
+// // setup variables
+// let loadedImageCount = 0;
+// let imageCount;
+// const container = document.querySelector('#main');
+
+// // seup Images loaded
+// const imgLoaded = imagesLoaded(container);
+// imageCount = imgLoaded.images.length;
+
+// // set the nitial progress to 0
+// updateProgress(0);
+
+// // tirggered after each item is loaded
+// imgLoaded.on('progress', () => {
+//   // increase the number of loaded images
+//   loadedImageCount++;
+
+//   // update progress
+//   updateProgress(loadedImageCount);
+// });
+
+// //  do whatever you want when all images are loaded
+// imgLoaded.on('done', () => {
+//   // init loader animation onComplete
+//   gsap.set(progressTween, { autoAlpha: 0, onComplete: initLoader });
+// });
+
+// function updateProgress(value) {
+//   gsap.to(progressTween, {
+//     progress: value / imageCount,
+//     duration: 0.3,
+//     ease: 'power1.out',
+//   });
+// }
+
+function initLoader() {
+  const tlLoader = gsap.timeline();
+  const tlLoaderIn = gsap.timeline({
+    defaults: { duration: 1.1, ease: 'power2.out' },
+    onComplete: () =>
+      document.querySelector('body').classList.remove('is-loading'),
+  });
+  const tlLoaderOut = gsap.timeline({
+    defaults: { duration: 1.2, ease: 'power2.inOut' },
+    delay: 1,
+  });
+
+  const image = document.querySelector('.loader__image img');
+  const mask = document.querySelector('.loader__image--mask');
+  const line1 = document.querySelector(
+    '.loader__title--mask:nth-child(1) span'
+  );
+  const line2 = document.querySelector(
+    '.loader__title--mask:nth-child(2) span'
+  );
+  const lines = document.querySelectorAll('.loader__title--mask');
+  const loader = document.querySelector('.loader');
+  const loaderContent = document.querySelector('.loader__content');
+
+  tlLoaderIn
+    .set([loader, loaderContent], { autoAlpha: 1 })
+    .to(
+      loaderInner,
+      { scaleY: 1, transformOrigin: 'bottom', ease: 'power2.inOut' },
+      0.2
+    )
+    .addLabel('revealImage')
+    .from(mask, { yPercent: 100 }, 'revealImage-=0.6')
+    .from(image, { yPercent: -50 }, 'revealImage-=0.6')
+    .from([line1, line2], { yPercent: 100, stagger: 0.1 }, 'revealImage-=0.4');
+
+  tlLoaderOut
+    .to(lines, { yPercent: -400, stagger: 0.2 })
+    .to([loader, loaderContent], { yPercent: -100 }, 0)
+    .from('#main', { y: 150 }, 0);
+
+  tlLoader.add(tlLoaderIn).add(tlLoaderOut);
+}
+
+/* 
+======
 INIT FUNCTION
 ======
 */
